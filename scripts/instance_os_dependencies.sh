a51b455b18809dd36fca9e03d94c47754e11b99b
tagoumyimlongmenestephane <tagstephane268@gmail.com>
2026-07-23 21:45:15 +0200

    fix: scripts clean_instance.sh

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# Mettre à jour et réparer les éventuels paquets cassés
-sudo apt-get update -y
-sudo apt-get install -f -y
+export DEBIAN_FRONTEND=noninteractive
 
-# Installer les dépendances système nécessaires
-sudo apt-get install -y python3-pip python3-venv ruby-full nginx
+sudo apt-get update -y
+sudo apt-get install -y python3-pip python3-venv python3-full nginx
