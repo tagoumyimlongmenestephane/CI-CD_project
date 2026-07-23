11cbc2ca45a3fbb864198253180003c8768ba52f
tagoumyimlongmenestephane <tagstephane268@gmail.com>
2026-07-23 21:07:05 +0200

    fix: scripts clean_instance.sh

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+set -e
 
-sudo apt install -y python3-pip
-sudo apt install -y nginx
-sudo apt install -y virtualenv
+# Mettre à jour et réparer les éventuels paquets cassés
+sudo apt-get update -y
+sudo apt-get install -f -y
+
+# Installer les dépendances système nécessaires
+sudo apt-get install -y python3-pip python3-venv ruby-full nginx
