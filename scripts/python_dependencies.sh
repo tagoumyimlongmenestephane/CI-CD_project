a638711a3e67b35754d597160f052ea6a64bca0d
tagoumyimlongmenestephane <tagstephane268@gmail.com>
2026-07-23 20:55:07 +0200

    fix: scripts clean_instance.sh

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-virtualenv /home/ubuntu/env
-source /home/ubuntu/env/bin/activate
+virtualenv /home/ubuntu/ven
+source /home/ubuntu/ven/bin/activate
 pip install -r /home/ubuntu/project_devops/requirements.txt
